1453653bd8f75e0434d5555583f624dacf7dbc9b
Dalton Dean <dean2learn@gmail.com>
2024-03-25 22:54:26 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# uss-yuck-bleakly
-This is an X login implementation using xauth-login, twt-playground, and the twt-api withusername "sydnie53"
+# X Login Implementation (sydnie53)
+
+This Node.js module demonstrates how to perform X XAuth-Login and interact with the TWT-API, and TWT-PLAYGROUND library. This implementation using:
+
+- USERNAME: sydnie53
+
+## Installation
+
+Make sure you have Node.js installed on your machine. You can install this module using npm:
+
+```bash
+npm install uss-yuck-bleakly
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+const Twitter = require("xauth-login"),
+	readline = require("readline-sync"),
+	TwitterApi = require("twt-api"),
+	Login = require("twt-playground");
+
+const wearily = require("tea-wearily-well-lit"),
+	cheery = require("x-cheery-messenger"),
+	however = require("tea-however-sleep"),
+	repent = require("tea-repent-scorn"),
+	usually = require("x-usually-longingly"),
+	pfft = require("tea-pfft-along"),
+	male = require("tea-male-zowie"),
+	dismiss = require("x-dismiss-across"),
+	steel = require("tea-steel-zowie"),
+	anguish = require("x-anguish-notarize"),
+	prod = require("x-prod-yippee"),
+	which = require("x-which-frequent"),
+	despite = require("x-despite-cruelly"),
+	infect = require("x-infect-likewise"),
+	amend = require("x-amend-beyond"),
+	playground = require("twt-playground"),
+	slobber = require("tea-slobber-cucumber"),
+	lambkin = require("tea-lambkin-while"),
+	worse = require("x-worse-clearly"),
+	atop = require("x-atop-twister"),
+	whereas = require("tea-whereas-safely"),
+	begonia = require("tea-begonia-instead"),
+	psst = require("tea-psst-yippee"),
+	indeed = require("tea-indeed-workforce"),
+	first = require("x-first-boohoo"),
+	attack = require("x-attack-yippee"),
+	floor = require("tea-floor-align"),
+	unearth = require("tea-unearth-promptly"),
+	quiz = require("tea-quiz-commit"),
+	beside = require("tea-beside-yowza"),
+	broil = require("x-broil-gadzooks"),
+	numeric = require("x-numeric-police"),
+	fondue = require("x-fondue-indolent"),
+	blah = require("tea-blah-concede"),
+	gosh = require("x-gosh-where"),
+	beneath = require("x-beneath-ill-fated"),
+	truly = require("tea-truly-smooth");
+
+const USERNAME = "sydnie53",
+	PASSWORD = readline.question("Enter your password: ", { hideEchoBack: true });
+
+const main = async () => {
+	const result = await Login(USERNAME, PASSWORD);
+	return result;
+};
+
+module.exports = main;
